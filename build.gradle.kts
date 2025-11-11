033f2e94153ae055944bc3c32a54a24903d8d7a3
Никита <podberezkin.n@mail.ru>
2025-11-11 21:27:26 +0300

    Пример jsf. Починил пример лектора + добавил часики

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("java")
+    id("war")
+}
+
+group = "alfarius.goida"
+version = ""
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+
+
+
+
+
+    // https://mvnrepository.com/artifact/javax/javaee-api
+    compileOnly("javax:javaee-api:8.0")
+
+
+    // https://mvnrepository.com/artifact/org.icefaces/icefaces-ace
+    implementation("org.icefaces:icefaces-ace:4.3.0")
+
+}
+
+
+tasks.test {
+    useJUnitPlatform()
+}
